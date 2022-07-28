3552777aa14cf0fbddfb578136520023221cad1b
RicoRodriges1 <vovagorianinxxx@gmail.com>
2022-07-28 17:10:23 +0300

    2

--- config.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
-const login = process.env.LOGIN
-const password = process.env.PASSWORD
-const port = process.env.PORT
-export {login, password, port}
-
